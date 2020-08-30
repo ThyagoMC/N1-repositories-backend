326341e43707ea3bd960dc4e1c671b767d1e69b1
Thyago Machado <thymac@gmail.com>
2020-08-30 13:07:14 -0300

    conceitos node.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require("uuid");
 
 const app = express();
 
@@ -11,23 +11,50 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+  repositories.push(repository);
+  response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const id = request.params.id;
+  const { title, url, techs } = request.body;
+  const repository = repositories.find((r) => r.id === id);
+  if (repository) {
+    repository.title = title;
+    repository.url = url;
+    repository.techs = techs;
+    response.json(repository);
+  } else {
+    response.status(400).json({ error: "Repository not found" });
+  }
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  let id = request.params.id;
+  let index = repositories.findIndex((r) => r.id === id);
+  if (index > -1) {
+    repositories.splice(index, 1);
+    response.sendStatus(204);
+  } else {
+    response.status(400).json({ error: "Repository not found" });
+  }
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  let id = request.params.id;
+  let repository = repositories.find((r) => r.id === id);
+  if (repository) {
+    repository.likes++;
+    response.json(repository);
+  } else {
+    response.status(400).json({ error: "Repository not found" });
+  }
 });
 
 module.exports = app;
